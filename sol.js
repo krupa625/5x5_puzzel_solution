1b0475b58276c0b0d65cf2f859df986a6284ad2e
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-02-11 23:06:02 +0530

    code success

---
+++ sol.js
@@ -0,0 +1,99 @@
+// // One star per row → No row has more than one star.
+// // One star per column → No column has more than one star.
+// // Stars don’t touch each other diagonally or adjacently.
+// // Each region can have only one star.
+// // Stars should not be at the edges.
+
+const regions = Array.from({ length: 5 }, () => Array(5).fill(0));
+
+const regionMap = {
+  Grey: [
+    [1, 1],
+    [0, 1],
+    [0, 0],
+    [1, 0],
+  ],
+  Red: [
+    [0, 3],
+    [1, 2],
+    [0, 2],
+  ],
+  Purple: [
+    [4, 4],
+    [0, 4],
+    [1, 4],
+    [2, 4],
+    [3, 4],
+    [3, 2],
+    [3, 3],
+    [4, 0],
+    [4, 1],
+    [4, 2],
+    [4, 3],
+  ],
+  Yellow: [
+    [2, 3],
+    [1, 3],
+    [2, 2],
+  ],
+  Blue: [
+    [3, 1],
+    [2, 0],
+    [3, 0],
+    [2, 1],
+    [2, 2],
+    [3, 2],
+  ],
+};
+
+function isValid(row, col, stars) {
+  for (let [r, c] of stars) {
+    if (r === row || c === col) 
+        return false;
+    if (Math.abs(r - row) <= 1 && Math.abs(c - col) <= 1) 
+        return false;//ye logic reference lekar kiya hai for check edges 
+  }
+  return true;
+}
+
+function placeStars(regionMap) {
+  let stars = [];
+  let placedRegions = new Set();
+
+  for (let color in regionMap) {
+    let positions = regionMap[color];
+    for (let [row, col] of positions) {
+      if (isValid(row, col, stars)) {
+        stars.push([row, col]);
+        regions[row][col] = "★";
+        placedRegions.add(color);
+        break;
+      }
+    }
+  }
+
+  for (let color in regionMap) {
+    if (!placedRegions.has(color)) {
+      let positions = regionMap[color];
+      for (let [row, col] of positions) {
+        if (isValid(row, col, stars)) {
+          stars.push([row, col]);
+          regions[row][col] = "★";
+
+          break;
+        }
+      }
+    }
+  }
+
+  for (let color in regionMap) {
+    if (!placedRegions.has(color)) {
+      let [row, col] = regionMap[color][0];
+      stars.push([row, col]);
+      regions[row][col] = "★";
+    }
+  }
+}
+
+placeStars(regionMap);
+console.log(regions);
